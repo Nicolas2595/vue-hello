e0875047cdfb5c5639af7ad330aedbf3a8ca8f1f
Nicolas2595 <nmorelli10@gmail.com>
2021-05-17 15:19:58 +0200

    vue-hello

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//Stampare a schermo un messaggio all'interno di un h1, utilizzando i data.
+//Aggiungere alla pagina un'immagine, presa anch'essa da un data.
+
+var app = new Vue(
+    
+    {
+        el: "#root",
+        data: {
+            message: "Hello Vue.JS!",
+            
+            linkImmagine: "https://assets.codepen.io/t-1003/internal/avatars/teams/default.png?fit=crop&format=auto&height=256&version=1513627136&width=256"
+        }
+        
+    }
+
+);
+
+
